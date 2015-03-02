0ec8ddb80359ae23b9894a3aae6dc564a8839409
Lynxaa <lynxaaog@gmail.com>
2015-03-02 11:39:57 +0800

    CSS Rework & JS fixes for Bootstrap 3.0

---
+++ bootstrap-custom/js/bootstrap-custom.js
@@ -0,0 +1,9 @@
+/* Bootstrap 3.0 Button Override
+------------------------------------------------------------------- */
+$(".btn").mouseup(function(){
+    $(this).blur();
+});
+
+$(".input").mouseup(function(){
+    $(this).blur();
+});
